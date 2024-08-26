8feb8a7da611158738d950e0473464ed73f8f6d7
kdbswo <kdbswo@gmail.com>
2024-08-26 17:11:23 +0900

    Feature: Hilt 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,4 +6,6 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ksp) apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+//    alias(libs.plugins.hilt) apply false
 }
