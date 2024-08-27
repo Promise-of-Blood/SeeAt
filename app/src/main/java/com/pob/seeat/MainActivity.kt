5dbd499c3b87e284fbc0fbdf5f0af9a92ab7a6c7
idkos <leegang31@gmail.com>
2024-08-27 21:59:32 +0900

    Fix: SystemBars 패딩 제거

--- app/src/main/java/com/pob/seeat/MainActivity.kt
+++ app/src/main/java/com/pob/seeat/MainActivity.kt
@@ -3,8 +3,6 @@ package com.pob.seeat
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.pob.seeat.databinding.ActivityMainBinding
@@ -16,11 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         initBottomNavigation()
     }
