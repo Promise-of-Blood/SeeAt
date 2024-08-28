787358be00b043eee6dcd03aed2a31336be0f9c9
Orinugoori <120167509+Orinugoori@users.noreply.github.com>
2024-08-28 11:37:29 +0900

    Update: 피드백 반영 수정
    Co-authored-by: 공명선 <84262085+APapeIsName@users.noreply.github.com>

--- app/src/main/java/com/pob/seeat/presentation/view/LoginActivity.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/LoginActivity.kt
@@ -55,6 +55,9 @@ class LoginActivity : AppCompatActivity() {
 
         val oAuthLoginCallback = object :OAuthLoginCallback{
             override fun onSuccess() {
+                    val name: String
+                    val email: String
+                    val gender: String
                 NidOAuthLogin().callProfileApi(object : NidProfileCallback<NidProfileResponse>{
                     override fun onSuccess(result: NidProfileResponse) {
                         name = result.profile?.name.toString()
