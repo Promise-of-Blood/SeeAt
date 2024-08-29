9e6cf70c73b3c3f8d4bb38598d45f9f5f84f51d5
APapeIsName <gms72901217@gmail.com>
2024-08-29 13:34:47 +0900

    Add: Chat List Adapter, Design: Link Bottom Navigation View

--- app/src/main/java/com/pob/seeat/presentation/view/chat/ChatListFragment.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/chat/ChatListFragment.kt
@@ -9,7 +9,11 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.pob.seeat.databinding.FragmentChatListBinding
 import com.pob.seeat.databinding.FragmentSampleBinding
+import com.pob.seeat.presentation.view.UiState
+import com.pob.seeat.presentation.view.chat.adapter.ChatListAdapter
+import com.pob.seeat.presentation.view.chat.items.ChatListUiItem
 import com.pob.seeat.presentation.viewmodel.SampleViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -17,68 +21,36 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ChatListFragment : Fragment() {
+    private val chatListAdapter = ChatListAdapter()
+
     companion object {
         fun newInstance() = MessageFragment
     }
 
-
-    private var _binding: FragmentSampleBinding? = null
+    private var _binding: FragmentChatListBinding? = null
     private val binding get() = _binding!!
-
-    private val sampleViewmodel: SampleViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentSampleBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentChatListBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sampleViewmodel.getSampleImageList(query = "검색어")
-
-        with(sampleViewmodel){
-            viewLifecycleOwner.lifecycleScope.launch {
-                sampleUiState.flowWithLifecycle(viewLifecycleOwner.lifecycle)
-                    .collectLatest {
-                        when(it){
-                            is UiState.Error ->{
-//                                binding.progressBar.isVisible = false
-                                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-                            }
-
-                            is UiState.Loading -> {
-//                                binding.progressBar.isVisible = true
-//                                binding.searchRecyclerview.isVisible = false
-                            }
-
-                            is UiState.Success -> {
-//                                binding.progressBar.isVisible = false
-//                                binding.searchRecyclerview.isVisible = true
-//                                searchListAdapter.submitList(it.data)
-                            }
-                        }
-                    }
-            }
-        }
-
+        binding.rvChatList.adapter = chatListAdapter
+        chatListAdapter.submitList(ChatListDummyData.getDummyList())
     }
-
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 
 }
--- app/src/main/java/com/pob/seeat/presentation/view/chat/MessageFragment.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/chat/MessageFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.pob.seeat.databinding.FragmentMessageBinding
 import com.pob.seeat.databinding.FragmentSampleBinding
 import com.pob.seeat.presentation.viewmodel.SampleViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,11 +22,8 @@ class MessageFragment : Fragment() {
         fun newInstance() = MessageFragment
     }
 
-
-    private var _binding: FragmentSampleBinding? = null
+    private var _binding: FragmentMessageBinding? = null
     private val binding get() = _binding!!
-
-    private val sampleViewmodel: SampleViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,49 +34,19 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSampleBinding.inflate(inflater, container, false)
+        _binding = FragmentMessageBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sampleViewmodel.getSampleImageList(query = "검색어")
-
-        with(sampleViewmodel){
-            viewLifecycleOwner.lifecycleScope.launch {
-                sampleUiState.flowWithLifecycle(viewLifecycleOwner.lifecycle)
-                    .collectLatest {
-                        when(it){
-                            is UiState.Error ->{
-//                                binding.progressBar.isVisible = false
-                                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-                            }
-
-                            is UiState.Loading -> {
-//                                binding.progressBar.isVisible = true
-//                                binding.searchRecyclerview.isVisible = false
-                            }
-
-                            is UiState.Success -> {
-//                                binding.progressBar.isVisible = false
-//                                binding.searchRecyclerview.isVisible = true
-//                                searchListAdapter.submitList(it.data)
-                            }
-                        }
-                    }
-            }
-        }
 
     }
-
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 
 }
