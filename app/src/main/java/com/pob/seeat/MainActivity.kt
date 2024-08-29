5f2808f65004d6b3e332bb01bd7ca1e50d7743ea
kdbswo <kdbswo@gmail.com>
2024-08-29 17:19:24 +0900

    Feature: 주석 제거 #24

--- app/src/main/java/com/pob/seeat/MainActivity.kt
+++ app/src/main/java/com/pob/seeat/MainActivity.kt
@@ -15,9 +15,6 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-//    @Inject
-//    lateinit var analytics: PresentLogRepository
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
