601515b37af633d438bc2e28c23d5e4ad787f6d4
idkos <leegang31@gmail.com>
2024-09-02 13:42:43 +0900

    Add: NewFeedFragment 생성 #24

---
+++ app/src/main/java/com/pob/seeat/presentation/view/feed/NewFeedFragment.kt
@@ -0,0 +1,87 @@
+package com.pob.seeat.presentation.view.feed
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.pob.seeat.R
+import com.pob.seeat.databinding.FragmentHomeBinding
+import com.pob.seeat.databinding.FragmentNewFeedBinding
+import com.pob.seeat.presentation.view.home.TagAdapter
+import com.pob.seeat.utils.Utils.tagList
+import dagger.hilt.android.AndroidEntryPoint
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
+import timber.log.Timber
+import java.util.Date
+
+
+class NewFeedFragment : Fragment() {
+    private var _binding: FragmentNewFeedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        uploadFeed()
+    }
+
+    private fun uploadFeed() {
+        binding.apply {
+            selectTag.setOnClickListener {
+                val modal = NewFeedModalBottomSheet()
+                modal.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
+                modal.show(parentFragmentManager, modal.tag)
+            }
+
+            tvUpload.setOnClickListener {
+                uploadFeed()
+                val feedData : HashMap<String, Any> = hashMapOf(
+                    "title" to etTitle.text.toString(),
+                    "content" to etContent.text.toString(),
+                    "date" to Timestamp(Date()),
+                    "tagList" to listOf("태그 1", "태그 2", "태그 3"),
+
+                    )
+                val db = Firebase.firestore
+                val ran = (0..10000).random()
+                db
+                    .collection("feed")
+                    .document("test_feed$ran")
+                    .set(feedData)
+                    .addOnSuccessListener { documentReference ->
+                        // 성공 시 처리
+                        Timber.d("DocumentSnapshot added with ID: $documentReference")
+                    }
+                    .addOnFailureListener { e ->
+                        // 실패 시 처리
+                        Timber.w(e, "Error adding document")
+                    }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
