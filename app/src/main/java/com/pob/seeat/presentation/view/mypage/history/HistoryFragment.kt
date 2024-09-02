fc0723a288ff0429fbe636340dd492236c502fa7
taeyoung <rlaxodud911@gmail.com>
2024-09-02 19:54:28 +0900

    Design: 댓글 목록 간격 늘림 #53

--- app/src/main/java/com/pob/seeat/presentation/view/mypage/history/HistoryFragment.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/mypage/history/HistoryFragment.kt
@@ -89,7 +89,7 @@ class HistoryFragment : Fragment() {
 
                         is UiState.Success -> {
                             binding.rvHistory.visibility = View.VISIBLE
-                            historyAdapter.submitList(response.data.take(5))
+                            historyAdapter.submitList(response.data.take(4))
                         }
                     }
                 }
@@ -98,7 +98,7 @@ class HistoryFragment : Fragment() {
 
     private fun getHistoryList() = when (position) {
         0 -> userHistoryViewModel.getUserFeedHistory(3)
-        1 -> userHistoryViewModel.getUserCommentHistory(5)
+        1 -> userHistoryViewModel.getUserCommentHistory(4)
         else -> userHistoryViewModel.getUserLikedHistory(3)
     }
 
