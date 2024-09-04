9cd23bfb67f980edb36c7f3665b5c5df90dda31a
taeyoung <rlaxodud911@gmail.com>
2024-09-04 19:49:05 +0900

    Refactor: 좋아요 목록을 불러오는 로직 변경 #71

--- app/src/main/java/com/pob/seeat/data/remote/UserHistoryRemote.kt
+++ app/src/main/java/com/pob/seeat/data/remote/UserHistoryRemote.kt
@@ -1,5 +1,6 @@
 package com.pob.seeat.data.remote
 
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.pob.seeat.domain.model.CommentHistoryModel
 import com.pob.seeat.domain.model.FeedModel
@@ -15,8 +16,7 @@ class UserHistoryRemote @Inject constructor(
         startAfter: String?
     ): List<FeedModel> {
         val userRef = firestore.collection("user").document(uid ?: "")
-        val feedDocuments = firestore.collection("feed")
-            .whereEqualTo("user", userRef)
+        val feedDocuments = firestore.collection("feed").whereEqualTo("user", userRef)
         if (limit != null) feedDocuments.limit(limit)
         if (startAfter != null) feedDocuments.startAfter(startAfter)
         return feedDocuments.get().await().documents.mapNotNull { documentSnapshot ->
@@ -34,15 +34,22 @@ class UserHistoryRemote @Inject constructor(
         startAfter: String? = null
     ): List<CommentHistoryModel> {
         val userRef = firestore.collection("user").document(uid ?: "")
-        val commentRef = firestore.collectionGroup("comments")
-            .whereEqualTo("user", userRef)
-        if (limit != null) commentRef.limit(limit)
-        return commentRef.get().await().documents.mapNotNull { documentSnapshot ->
-            val feedDocument = documentSnapshot.reference.parent.parent?.get()?.await()
-            documentSnapshot.toObject(CommentHistoryModel::class.java)?.copy(
-                feedId = feedDocument?.id ?: "",
-                feedTitle = feedDocument?.get("title") as? String ?: "",
+        val commentsDocuments = firestore.collectionGroup("comments").whereEqualTo("user", userRef)
+        val feedDocumentsMap =
+            mutableMapOf<String, DocumentSnapshot>() // 같은 글에 여러 개의 댓글을 단 경우 중복 요청 방지
+        if (limit != null) commentsDocuments.limit(limit)
+        return commentsDocuments.get().await().documents.mapNotNull { documentSnapshot ->
+            val feedId = documentSnapshot.reference.parent.parent?.id ?: ""
+            val feedDocument =
+                feedDocumentsMap[feedId] ?: firestore.collection("feed").document(feedId)
+                    .get().await().also { document -> feedDocumentsMap[feedId] = document }
+            CommentHistoryModel(
+                feedId = feedId,
+                feedTitle = feedDocument.getString("title") ?: "",
                 commentId = documentSnapshot.id,
+                comment = documentSnapshot.getString("comment") ?: "",
+                likeCount = documentSnapshot.get("likeCount") as? Int ?: 0,
+                timeStamp = documentSnapshot.getTimestamp("timeStamp")
             )
         }
     }
@@ -53,16 +60,15 @@ class UserHistoryRemote @Inject constructor(
         startAfter: String? = null
     ): List<FeedModel> {
         val userRef = firestore.collection("user").document(uid ?: "")
-        val feedRefs = firestore.collection("like").whereEqualTo("user", userRef)
-        if (limit != null) feedRefs.limit(limit)
-        if (startAfter != null) feedRefs.startAfter(startAfter)
-        return feedRefs.get().await().documents.mapNotNull { documentSnapshot ->
-            val feedDocument =
-                firestore.collection("feed").document(documentSnapshot.id).get().await()
-            val tagList = documentSnapshot.get("tagList") as? List<*>
-            feedDocument.toObject(FeedModel::class.java)?.copy(
-                feedId = feedDocument.id,
-                tags = tagList?.filterIsInstance<String>() ?: emptyList()
+        val likedFeedRefs = firestore.collection("user").document(uid ?: "").collection("likedFeed")
+        if (limit != null) likedFeedRefs.limit(limit)
+        if (startAfter != null) likedFeedRefs.startAfter(startAfter)
+        return likedFeedRefs.get().await().documents.mapNotNull { documentSnapshot ->
+            val feedDocument = documentSnapshot.getDocumentReference("feed")?.get()?.await()
+            feedDocument?.toObject(FeedModel::class.java)?.copy(
+                feedId = documentSnapshot.id,
+                tags = (documentSnapshot.get("tagList") as? List<*>)?.filterIsInstance<String>()
+                    .orEmpty(),
             )
         }
     }
