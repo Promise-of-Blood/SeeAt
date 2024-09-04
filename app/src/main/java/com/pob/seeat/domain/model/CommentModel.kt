1e51790089ddfad86e956848081b3d42be1ef456
taeyoung <rlaxodud911@gmail.com>
2024-09-04 11:37:05 +0900

    Remove: FeedModel과 CommentModel의 Parcelable 구현부 제거 #58

--- app/src/main/java/com/pob/seeat/domain/model/CommentModel.kt
+++ app/src/main/java/com/pob/seeat/domain/model/CommentModel.kt
@@ -1,13 +1,9 @@
 package com.pob.seeat.domain.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.parcelize.RawValue
 
-//@Parcelize
 data class CommentModel(
     val commentId: String = "",
     val user: @RawValue DocumentReference? = null,
@@ -16,41 +12,4 @@ data class CommentModel(
     val timeStamp: Timestamp? = null,
     val userImage: String = "",
     val userNickname: String = ""
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        commentId = parcel.readString() ?: "",
-        // TODO parcelize 방식 개선
-        user = parcel.readString()?.let {
-            FirebaseFirestore.getInstance().collection("user").document(it.substringAfterLast("/"))
-        },
-        comment = parcel.readString() ?: "",
-        likeCount = parcel.readInt(),
-        timeStamp = parcel.readParcelable(Timestamp::class.java.classLoader),
-        userImage = parcel.readString() ?: "",
-        userNickname = parcel.readString() ?: "",
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(commentId)
-        parcel.writeString(user?.path)
-        parcel.writeString(comment)
-        parcel.writeInt(likeCount)
-        parcel.writeParcelable(timeStamp, flags)
-        parcel.writeString(userImage)
-        parcel.writeString(userNickname)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<CommentModel> {
-        override fun createFromParcel(parcel: Parcel): CommentModel {
-            return CommentModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CommentModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
--- app/src/main/java/com/pob/seeat/domain/model/FeedModel.kt
+++ app/src/main/java/com/pob/seeat/domain/model/FeedModel.kt
@@ -1,14 +1,10 @@
 package com.pob.seeat.domain.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
 import kotlinx.parcelize.RawValue
 
-//@Parcelize
 data class FeedModel(
     val feedId: String = "",
     val user: @RawValue DocumentReference? = null,
@@ -23,62 +19,4 @@ data class FeedModel(
     val tags: List<String> = emptyList(),
     val userImage: String = "",
     val contentImage: List<String> = emptyList()
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        feedId = parcel.readString() ?: "",
-        // TODO parcelize 방식 개선
-        user = parcel.readString()?.let {
-            FirebaseFirestore.getInstance().collection("user").document(it.substringAfterLast("/"))
-        },
-        nickname = parcel.readString() ?: "",
-        title = parcel.readString() ?: "",
-        content = parcel.readString() ?: "",
-        like = parcel.readInt(),
-        commentsCount = parcel.readInt(),
-        location = parcel.readDouble().let { latitude ->
-            val longitude = parcel.readDouble()
-            if (latitude != 0.0 && longitude != 0.0) GeoPoint(latitude, longitude) else null
-        },
-        date = parcel.readParcelable(Timestamp::class.java.classLoader),
-        comments = parcel.createTypedArrayList(CommentModel.CREATOR) ?: emptyList(),
-        tags = parcel.createStringArrayList().orEmpty(),
-        userImage = parcel.readString() ?: "",
-        contentImage = parcel.createStringArrayList().orEmpty(),
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(feedId)
-        parcel.writeString(user?.path)
-        parcel.writeString(nickname)
-        parcel.writeString(title)
-        parcel.writeString(content)
-        parcel.writeInt(like)
-        parcel.writeInt(commentsCount)
-        if (location != null) {
-            parcel.writeDouble(location.latitude)
-            parcel.writeDouble(location.longitude)
-        } else {
-            parcel.writeDouble(0.0)
-            parcel.writeDouble(0.0)
-        }
-        parcel.writeParcelable(date, flags)
-        parcel.writeTypedList(comments)
-        parcel.writeStringList(tags)
-        parcel.writeString(userImage)
-        parcel.writeStringList(contentImage)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<FeedModel> {
-        override fun createFromParcel(parcel: Parcel): FeedModel {
-            return FeedModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<FeedModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
