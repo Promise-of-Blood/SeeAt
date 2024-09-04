21c997004ce9e27863fb8cbf58f9f6298233d771
taeyoung <rlaxodud911@gmail.com>
2024-09-04 18:41:46 +0900

    Design: 태그 목록 삭제 #71

--- app/src/main/java/com/pob/seeat/presentation/view/mypage/history/HistoryAdapter.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/mypage/history/HistoryAdapter.kt
@@ -2,23 +2,17 @@ package com.pob.seeat.presentation.view.mypage.history
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources.getColorStateList
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
-import com.pob.seeat.R
 import com.pob.seeat.databinding.ItemCommentHistoryBinding
 import com.pob.seeat.databinding.ItemFeedHistoryBinding
 import com.pob.seeat.presentation.common.ViewHolder
 import com.pob.seeat.presentation.view.mypage.items.HistoryEnum
 import com.pob.seeat.presentation.view.mypage.items.HistoryListItem
-import com.pob.seeat.utils.Utils.px
 import com.pob.seeat.utils.Utils.toFormatShortenedString
-import com.pob.seeat.utils.Utils.toTagList
 
 class HistoryAdapter : ListAdapter<HistoryListItem, ViewHolder<HistoryListItem>>(object :
     DiffUtil.ItemCallback<HistoryListItem>() {
@@ -64,7 +58,6 @@ class HistoryAdapter : ListAdapter<HistoryListItem, ViewHolder<HistoryListItem>>
 
     class FeedViewHolder(binding: ItemFeedHistoryBinding) :
         ViewHolder<HistoryListItem>(binding.root) {
-        private val tagContainer = binding.cgFeedTagContainer
         private val title = binding.tvFeedTitle
         private val content = binding.tvFeedContent
         private val commentCount = binding.tvFeedCommentCount
@@ -79,36 +72,10 @@ class HistoryAdapter : ListAdapter<HistoryListItem, ViewHolder<HistoryListItem>>
                 commentCount.text = feed.commentCount.toFormatShortenedString()
                 likeCount.text = feed.likeCount.toFormatShortenedString()
                 time.text = feed.time
-
-                tagContainer.removeAllViews()
-                tagContainer.addFeedTags(feed.tagList)
                 Glide.with(itemView.context)
                     .load(feed.image)
                     .apply(RequestOptions.bitmapTransform(RoundedCorners(10)))
                     .into(image)
-            }
-        }
-
-        private fun ChipGroup.addFeedTags(tags: List<String>) {
-            for (tag in tags.toTagList()) {
-                val chip = Chip(context).apply {
-                    text = tag.tagName
-                    textSize = 12f
-                    textEndPadding = 4f.px.toFloat()
-
-                    setChipIconResource(tag.tagImage)
-                    chipBackgroundColor = getColorStateList(context, R.color.white)
-                    chipStrokeWidth = 0f
-                    chipIconSize = 12f.px.toFloat()
-                    chipMinHeight = 24f.px.toFloat()
-                    chipCornerRadius = 32f.px.toFloat()
-                    iconStartPadding = 4f.px.toFloat()
-
-                    elevation = 3f.px.toFloat()
-                    isCheckable = false
-                    isClickable = false
-                }
-                this.addView(chip)
             }
         }
     }
