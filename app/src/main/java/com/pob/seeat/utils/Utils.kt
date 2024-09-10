bc52230cf9a8280a7d68e0219ce96d67d6fe1f3d
taeyoung <rlaxodud911@gmail.com>
2024-09-10 16:25:46 +0900

    Feature: 날짜 표시 형식 변경

--- app/src/main/java/com/pob/seeat/utils/Utils.kt
+++ app/src/main/java/com/pob/seeat/utils/Utils.kt
@@ -29,39 +29,47 @@ object Utils {
     /**
      * 현재 시간으로부터 기준 시간과의 차이를 한국어로 반환합니다.
      *
-     * - 0~59초: 방금 전
-     * - 60~3599초: n분 전
-     * - 3600~86399초: n시간 전
-     * - 86400~604799초: n일 전
-     * - 이외에는 "yyyy.MM.dd" 형식의 문자열을 반환합니다.
+     * - 1분 미만: 방금 전
+     * - 1분 이상 1시간 미만: n분 전
+     * - 1시간 이상 하루 미만: n시간 전
+     * - 하루 이상 일주일 미만: n일 전
+     * - 일주일 이상 한달 미만: n주 전
+     * - 한달 이상 1년 미만: n달 전
+     * - 1년 이상: n년 전
      * */
     fun LocalDateTime.toKoreanDiffString(): String {
         return when (val diffSec = Duration.between(this, LocalDateTime.now()).seconds) {
-            in 0..59 -> "방금 전"
-            in 60..3599 -> "${diffSec / 60}분 전"
-            in 3600..86399 -> "${diffSec / 3600}시간 전"
-            in 86400..604799 -> "${diffSec / 86400}일 전"
-            else -> this.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+            in 0..59 -> "방금 전" // ~ 60
+            in 60..3599 -> "${diffSec / 60}분 전" // ~ 60 * 60
+            in 3600..86399 -> "${diffSec / 3600}시간 전" // ~ 60 * 60 * 24
+            in 86400..604799 -> "${diffSec / 86400}일 전" // ~ 60 * 60 * 24 * 7
+            in 604800..2419199 -> "${diffSec / 604800}주 전" // ~ 60 * 60 * 24 * 7 * 4
+            in 2419200..31535999 -> "${diffSec / 2592000}달 전" // ~ 60 * 60 * 24 * 365
+            else -> "${diffSec / 31536000}년 전"
         }
     }
 
     /**
      * 현재 시간으로부터 기준 시간과의 차이를 한국어로 반환합니다.
      *
-     * - 0~59초: 방금 전
-     * - 60~3599초: n분 전
-     * - 3600~86399초: n시간 전
-     * - 86400~604799초: n일 전
-     * - 이외에는 "yy.MM.dd" 형식의 문자열을 반환합니다.
+     * - 1분 미만: 방금 전
+     * - 1분 이상 1시간 미만: n분 전
+     * - 1시간 이상 하루 미만: n시간 전
+     * - 하루 이상 일주일 미만: n일 전
+     * - 일주일 이상 한달 미만: n주 전
+     * - 한달 이상 1년 미만: n달 전
+     * - 1년 이상: n년 전
      * */
     fun Timestamp.toKoreanDiffString(): String {
         val diffSec = TimeUnit.MILLISECONDS.toSeconds(Date().time - this.toDate().time)
         return when (diffSec) {
-            in 0..59 -> "방금 전"
-            in 60..3599 -> "${diffSec / 60}분 전"
-            in 3600..86399 -> "${diffSec / 3600}시간 전"
-            in 86400..604799 -> "${diffSec / 86400}일 전"
-            else -> SimpleDateFormat("yy.MM.dd", Locale.getDefault()).format(this.toDate())
+            in 0..59 -> "방금 전" // ~ 60
+            in 60..3599 -> "${diffSec / 60}분 전" // ~ 60 * 60
+            in 3600..86399 -> "${diffSec / 3600}시간 전" // ~ 60 * 60 * 24
+            in 86400..604799 -> "${diffSec / 86400}일 전" // ~ 60 * 60 * 24 * 7
+            in 604800..2419199 -> "${diffSec / 604800}주 전" // ~ 60 * 60 * 24 * 7 * 4
+            in 2419200..31535999 -> "${diffSec / 2592000}달 전" // ~ 60 * 60 * 24 * 365
+            else -> "${diffSec / 31536000}년 전"
         }
     }
 
