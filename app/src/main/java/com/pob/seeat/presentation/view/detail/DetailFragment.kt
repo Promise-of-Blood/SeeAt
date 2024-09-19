24d8a1a6eb68a036a72f8ef3dd3dc0c8241aab5e
kdbswo <kdbswo@gmail.com>
2024-09-19 12:00:27 +0900

    Feature: 게시글 수정 버튼 추가 #162

--- app/src/main/java/com/pob/seeat/presentation/view/detail/DetailFragment.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/detail/DetailFragment.kt
@@ -26,6 +26,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
@@ -88,7 +89,7 @@ class DetailFragment : Fragment() {
     private lateinit var feed: FeedModel
     private lateinit var chatId : String
 
-    private val detailViewModel: DetailViewModel by viewModels()
+    private val detailViewModel: DetailViewModel by activityViewModels()
     private val commentViewModel: CommentViewModel by viewModels()
     private val reportCommentViewModel: ReportCommentViewModel by viewModels()
     private val chatViewModel : ChatViewModel by viewModels()
@@ -144,6 +145,14 @@ class DetailFragment : Fragment() {
 
                 override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                     return when (menuItem.itemId) {
+
+                        R.id.feed_edit -> {
+
+                            findNavController().navigate(R.id.action_navigation_detail_to_navigation_detail_edit)
+
+                            true
+                        }
+
                         R.id.feed_remove -> {
 
                             detailViewModel.removeFeed(feed.feedId)
