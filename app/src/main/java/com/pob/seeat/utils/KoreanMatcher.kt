2d90a7822bcf9f79709f0a6a7d56aca40e3c137c
taeyoung <rlaxodud911@gmail.com>
2024-09-19 16:46:24 +0900

    Refactor: 한글 검색 기능 개선
    - "구"를 검색해도 "국"이나 "굿"이 검색되게 구현

--- app/src/main/java/com/pob/seeat/utils/KoreanMatcher.kt
+++ app/src/main/java/com/pob/seeat/utils/KoreanMatcher.kt
@@ -4,6 +4,7 @@ object KoreanMatcher {
     private const val KOREAN_UNICODE_START = 44032 // 가
     private const val KOREAN_UNICODE_END = 55203   // 힣
     private const val KOREAN_UNICODE_BASED = 588   // 각 자음 마다 가지는 글자 수
+    private const val KOREAN_JONGSEONG_COUNT = 28 // 받침의 개수
     private val koreanConsonant = arrayOf(
         'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ',
         'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
@@ -20,6 +21,19 @@ object KoreanMatcher {
         val hasBegin = (char.code - KOREAN_UNICODE_START)
         val idx = hasBegin / KOREAN_UNICODE_BASED
         return koreanConsonant[idx]
+    }
+
+    // 받침 제거
+    private fun removeJongseong(char: Char): Char {
+        if (!isKorean(char)) return char // 한글이 아닌 경우 그대로 반환
+
+        val idx = (char.code - KOREAN_UNICODE_START) % KOREAN_JONGSEONG_COUNT
+        return if (idx == 0) {
+            char // 받침이 없는 경우 그대로 반환
+        } else {
+            // 받침이 있는 경우 받침을 제거한 문자 반환
+            ((char.code - idx).toChar())
+        }
     }
 
     /**
@@ -39,17 +53,20 @@ object KoreanMatcher {
                 temp = 0
 
                 while (temp < searchLength) {
+                    val basedChar = removeJongseong(based[i + temp])
+
+                    // 현재 char이 초성이고 based가 한글일 때
                     if (isConsonant(search[temp]) && isKorean(based[i + temp])) {
-                        // 현재 char이 초성이고 based가 한글일 때
+                        // 각각의 초성끼리 같은지 비교
                         if (getConsonant(based[i + temp]) == search[temp]) {
-                            // 각각의 초성끼리 같은지 비교
                             temp++
                         } else {
                             break
                         }
                     } else {
-                        // char이 초성이 아니라면
-                        if (based[i + temp] == search[temp]) {
+                        // char이 일반 문자라면
+                        // 문자가 같거나, 받침을 제외한 문자가 같은지 비교
+                        if (based[i + temp] == search[temp] || basedChar == search[temp]) {
                             temp++
                         } else {
                             break
