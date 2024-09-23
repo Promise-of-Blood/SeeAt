502fef0b05ae95b2d1711de14a774d81018db76f
taeyoung <rlaxodud911@gmail.com>
2024-09-23 21:33:11 +0900

    Feature: 관리자 페이지 navigation graph 작성 #163

--- app/src/main/java/com/pob/seeat/presentation/view/admin/AdminActivity.kt
+++ app/src/main/java/com/pob/seeat/presentation/view/admin/AdminActivity.kt
@@ -5,10 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.pob.seeat.R
 import com.pob.seeat.databinding.ActivityAdminBinding
-import com.pob.seeat.presentation.view.admin.adapter.AdminViewPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,15 +28,23 @@ class AdminActivity : AppCompatActivity() {
     }
 
     private fun initView() = with(binding) {
-        vpAdmin.adapter = AdminViewPagerAdapter(this@AdminActivity)
-        TabLayoutMediator(tlAdmin, vpAdmin) { tab, position ->
-            when (position) {
-                0 -> tab.text = getString(R.string.admin_tab_user)
-                1 -> tab.text = getString(R.string.admin_tab_report)
-            }
-        }.attach()
-
         setSupportActionBar(tbAdmin)
         ivAdminClose.setOnClickListener { finish() }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_admin) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            ivAdminTitle.text = destination.label
+        }
+        ivAdminLogo.setOnClickListener { navController.navigate(R.id.admin) }
+    }
+
+    fun setNavigateButton() = with(binding) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        tbAdmin.setNavigationOnClickListener {
+            findNavController(R.id.fcv_admin).popBackStack()
+        }
     }
 }
---
+++ app/src/main/java/com/pob/seeat/presentation/view/admin/AdminFragment.kt
@@ -0,0 +1,46 @@
+package com.pob.seeat.presentation.view.admin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.pob.seeat.R
+import com.pob.seeat.databinding.FragmentAdminBinding
+import com.pob.seeat.presentation.view.admin.adapter.AdminViewPagerAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AdminFragment : Fragment() {
+    private var _binding: FragmentAdminBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAdminBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun initView() = with(binding) {
+        vpAdmin.adapter = AdminViewPagerAdapter(requireActivity())
+        TabLayoutMediator(tlAdmin, vpAdmin) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.admin_tab_user)
+                1 -> tab.text = getString(R.string.admin_tab_report)
+            }
+        }.attach()
+    }
+}
