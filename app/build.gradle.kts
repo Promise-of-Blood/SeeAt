fc8e33b4fe42b8163f5e40ee36eda45d859bf64b
overtae <rlaxodud911@gmail.com>
2024-12-09 23:43:59 +0900

    Chore: 씨앗 v1.1.1 출시용 버전 코드 변경 (3 > 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.pob.seeat"
         minSdk = 30
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
